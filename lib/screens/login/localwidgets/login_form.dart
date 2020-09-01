ab11c59ce918e5dfb2fdd88ba14c07db284496f8
jhsc <hi@jhsc.io>
2020-09-01 15:11:20 -0700

    login method

--- lib/screens/login/localwidgets/login_form.dart
+++ lib/screens/login/localwidgets/login_form.dart
@@ -1,8 +1,43 @@
+import 'package:book_dub/screens/home/home.dart';
 import 'package:book_dub/screens/signup/signup.dart';
+import 'package:book_dub/states/currentUser.dart';
 import 'package:book_dub/widgets/our_container.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class OurLoginForm extends StatelessWidget {
+class OurLoginForm extends StatefulWidget {
+  @override
+  _OurLoginFormState createState() => _OurLoginFormState();
+}
+
+class _OurLoginFormState extends State<OurLoginForm> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _loginUser(BuildContext context, String email, password) async {
+    CurrentUser _currentUser = Provider.of<CurrentUser>(context, listen: false);
+
+    try {
+      if (await _currentUser.loginUser(email, password)) {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(),
+            ),
+            (route) => false);
+      } else {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Incorrect Login information..."),
+            duration: Duration(seconds: 3),
+          ),
+        );
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return OurContainer(
@@ -23,7 +58,7 @@ class OurLoginForm extends StatelessWidget {
             ),
           ),
           TextFormField(
-            // controller: _emailController,
+            controller: _emailController,
             decoration: InputDecoration(
               prefixIcon: Icon(Icons.alternate_email),
               hintText: "Email",
@@ -33,7 +68,7 @@ class OurLoginForm extends StatelessWidget {
             height: 20.0,
           ),
           TextFormField(
-            // controller: _passwordController,
+            controller: _passwordController,
             decoration: InputDecoration(
               prefixIcon: Icon(Icons.lock_outline),
               hintText: "Password",
@@ -56,7 +91,7 @@ class OurLoginForm extends StatelessWidget {
               ),
             ),
             onPressed: () {
-              print("Login pressed");
+              _loginUser(context, _emailController.text, _emailController.text);
             },
           ),
           FlatButton(
